1fd145c85fa353dcd704115794923210e07daa20
Javi <javi.cb@gmail.com>
2021-01-13 13:00:45 +0100

    adding sarch functionality

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
+    <input type="text" placeholder="Search Books" v-model="searchInput" />
     <ul>
-      <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
+      <book-item
+        v-for="book in searchedBooks"
+        :key="book.id"
+        :book="book"
+      ></book-item>
     </ul>
     <hr />
     <h2>Filtered Books By Ownership</h2>
@@ -57,6 +62,7 @@ export default {
       ],
       filters: ["bought", "borrowed"],
       holding: "bought",
+      searchInput: "",
     };
   },
   components: {
@@ -66,6 +72,12 @@ export default {
   computed: {
     filteredBooks() {
       return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = (book) => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      };
+      return _.filter(this.books, searchFilter);
     },
   },
   methods: {
